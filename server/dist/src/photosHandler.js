9c35a1f635687f179886942b292b081a49cb4bad
Kuti <kutigolberg@gmail.com>
2021-12-24 17:12:43 +0200

    added server dist to git ignore

--- server/dist/src/photosHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.processPhotosConfig = void 0;
-//{targetProperties, sources}: PhotosInput
-function processPhotosConfig() {
-    return "fuck";
-}
-exports.processPhotosConfig = processPhotosConfig;
-//# sourceMappingURL=photosHandler.js.map
